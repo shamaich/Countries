2a3890530978ed55ae23a8612f8b0c1a17debc74
shamaich <shamaevr@ya.ru>
2019-12-02 03:30:28 -0800

    v1.5 UI

--- app/src/main/java/com/example/countries/view/DetailFragment.java
+++ app/src/main/java/com/example/countries/view/DetailFragment.java
@@ -2,10 +2,13 @@ package com.example.countries.view;
 
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -19,13 +22,22 @@ import com.example.countries.databinding.FragmentDetailBinding;
 import com.example.countries.model.Country;
 import com.example.countries.viewmodel.ListViewModel;
 
+import android.widget.LinearLayout.LayoutParams;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class DetailFragment extends Fragment {
 
     private static Country country;
     private FragmentDetailBinding binding;
     private ListViewModel listViewModel;
-    private Button saveCountryButton;
+    @BindView(R.id.fragment_detail_save_button)
+    Button saveCountryButton;
 
     public DetailFragment() {
     }
@@ -35,7 +47,6 @@ public class DetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         FragmentDetailBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false);
-
         this.binding = binding;
         return binding.getRoot();
     }
@@ -44,12 +55,17 @@ public class DetailFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         assert getArguments() != null;
+        ButterKnife.bind(this, view);
         country = DetailFragmentArgs.fromBundle(getArguments()).getCountry();
         binding.setCountry(country);
         listViewModel = ViewModelProviders.of(this).get(ListViewModel.class);
-        saveCountryButton = view.findViewById(R.id.fragment_detail_save_button);
-        saveCountryButton.setOnClickListener(view1 -> listViewModel.insertCountry(country));
+        ;
+        saveCountryButton.setOnClickListener(view1 -> {
+            listViewModel.insertCountry(country);
+            saveCountryButton.setEnabled(false);
+        });
 
+        initializeLayouts();
         observeViewModel();
 
     }
@@ -58,12 +74,61 @@ public class DetailFragment extends Fragment {
         listViewModel.connectionLiveData.observe(getViewLifecycleOwner(), connection -> {
             if (connection.getIsConnected()) {
                 saveCountryButton.setVisibility(View.VISIBLE);
-                Toast.makeText(getContext(), String.format("Wireless connection"), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "Wireless connection", Toast.LENGTH_SHORT).show();
             } else {
                 saveCountryButton.setVisibility(View.INVISIBLE);
-                Toast.makeText(getContext(), String.format("Connection turned OFF"), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "Connection turned OFF", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void initializeLayouts() {
+
+        LinearLayout linearLayout = Objects.requireNonNull(getView()).findViewById(R.id.fragment_detail_layout_callingCodes);
+        LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+        layoutParams.gravity = Gravity.CENTER_HORIZONTAL;
+        StringBuilder getCountryTimezone = new StringBuilder();
+        StringBuilder getCountryCurrency = new StringBuilder();
+        StringBuilder getCountryLanguages = new StringBuilder();
+
+        for (String x : country.getTimezones()) {
+            getCountryTimezone.append("\n").append(x);//.append(" ");
+        }
+
+        TextView textViewTimeZone = new TextView(getContext());
+        textViewTimeZone.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT));
+        textViewTimeZone.setPadding(10, 10, 10, 10);
+        textViewTimeZone.setLayoutParams(layoutParams);
+        textViewTimeZone.setText(String.format("Timezones: %s", getCountryTimezone));
+        linearLayout.addView(textViewTimeZone);
+
+        for (HashMap<String, String> x : country.getCurrencies()) {
+            getCountryCurrency.append("\n").append("Name: ").append(x.get("name"));
+        }
+
+        TextView textViewCurrency = new TextView(getContext());
+        textViewCurrency.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT));
+        textViewCurrency.setPadding(10, 10, 10, 10);
+        textViewCurrency.setLayoutParams(layoutParams);
+        textViewCurrency.setText(String.format("Currency:%s", getCountryCurrency));
+        linearLayout.addView(textViewCurrency);
+
+
+        for (HashMap<String, String> x : country.getLanguages()) {
+            getCountryLanguages.append("\n").append("Name: ").append(x.get("name"));
+        }
+
+        TextView textViewLanguages = new TextView(getContext());
+        textViewLanguages.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT));
+        textViewLanguages.setPadding(10, 10, 10, 10);
+        textViewLanguages.setLayoutParams(layoutParams);
+        textViewLanguages.setText(String.format("Languages:%s", getCountryLanguages));
+        linearLayout.addView(textViewLanguages);
+
+
     }
 
 
